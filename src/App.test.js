ade43b8fa1936c655b20430252bdf43f59ac4337
tatiana <tgallon@conocimientocorporativo.com>
2023-04-17 18:37:14 -0500

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// Este es el archivo de prueba que usa testing library
+// Importamos las dependencias necesarias
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// Escribimos una prueba que verifica que el componente se renderiza correctamente
+test("renderiza el componente Counter", () => {
+  // Usamos el método render para montar el componente en el DOM virtual
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Usamos el método screen para acceder al DOM
+  // Usamos el método getByText para buscar el elemento que tiene el texto 'El valor actual es 0'
+  const counterElement = screen.getByText(/El valor actual es 0/i);
+  // Esperamos que el elemento exista
+  expect(counterElement).toBeInTheDocument();
 });
+
+// Escribimos otra prueba que verifica que el botón incrementa el valor
+test("incrementa el valor al hacer clic en el botón", () => {
+  // Usamos el método render para montar el componente en el DOM virtual
+  render(<App />);
+  // Usamos el método screen para acceder al DOM
+  // Usamos el método getByText para buscar el elemento que tiene el texto 'Incrementar'
+  const buttonElement = screen.getByText(/Incrementar/i);
+  // Simulamos un clic en el botón usando el método fireEvent
+  fireEvent.click(buttonElement);
+  // Usamos el método getByText para buscar el elemento que tiene el texto 'El valor actual es 1'
+  const counterElement = screen.getByText(/El valor actual es 1/i);
+  // Esperamos que el elemento exista
+  expect(counterElement).toBeInTheDocument();
+});
+
+test("Encontrar un boton dentro del DOM", ()=>{
+  render (<App/>)
+
+  const input = screen.getByText(/El valor actual es/i);
+  expect(input).toBeInTheDocument();
+})
